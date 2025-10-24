1c62f0dfbe3825d14c6f83957d47076624a4865a
raihan1846 <raihan.bd2022@gmail.com>
2025-10-24 22:20:15 +0600

    install all package and design navbar

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCR9IkZRXVCFhHbCgdJjB-ni_T9hKkYLZ4",
+  authDomain: "greennest-ef4bc.firebaseapp.com",
+  projectId: "greennest-ef4bc",
+  storageBucket: "greennest-ef4bc.firebasestorage.app",
+  messagingSenderId: "633111956087",
+  appId: "1:633111956087:web:0a058409f17c0b2ca35472"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from '../components/Nabvar/Navbar';
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <header className='w-11/12 mx-auto my-3'>
+             <Navbar></Navbar>
+            </header>
+            <main>
+
+            </main>
+            <footer>
+
+            </footer>
+        </div>
+    );
+};
+
+export default HomeLayout;
